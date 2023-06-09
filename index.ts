1625826ecafc4756f2a0899a71cf1c92a5dd9dbc
ManikandanKumarkp <87644503+ManikandanKumarkp@users.noreply.github.com>
2023-06-09 15:07:46 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,16 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+interface person{
+  name:string;
+  age:number;
+  greet:() => void;
+}
+const Mani: person = {
+name:"Mani",
+age:25,
+greet(){
+  console.log("hello,my name is ", Mani)
+}
+};
